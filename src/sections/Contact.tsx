dc743637e528c285a6c0583a838c2b97e43f9c62
Horacio Iann Toquiantzi <ianntoquiantzi020419@hotmail.com>
2026-01-19 13:24:32 -0600

    feat: Added animations in Contact section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,6 +1,8 @@
+import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
+
 function Contact() {
   return (
-    <section className="contact-section">
+    <section className="contact-section" data-aos="fade-up" data-aos-duration="1200">
       <h2 className="contact-title">Contact</h2>
 
       <p className="contact-description">
@@ -10,14 +12,17 @@ function Contact() {
 
       <div className="contact-links">
         <a href="mailto:ianntoquiantzi020419@hotmail.com" className="contact-btn email">
+          <FaEnvelope size={18} style={{ marginRight: "8px" }} />
           Email Me
         </a>
 
         <a href="https://github.com/IannToquiantzi" target="_blank" className="contact-btn github">
+          <FaGithub size={18} style={{ marginRight: "8px" }} />
           GitHub
         </a>
 
         <a href="https://www.linkedin.com/in/horacio-iann/" target="_blank" className="contact-btn linkedin">
+          <FaLinkedin size={18} style={{ marginRight: "8px" }} />
           LinkedIn
         </a>
       </div>
@@ -26,4 +31,5 @@ function Contact() {
 }
 
 export default Contact;
+
 
